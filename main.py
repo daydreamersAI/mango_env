c0e741ea57e88043659a0264931f9b940bc2f29b
vieveks <vivekp.padman@gmail.com>
2023-12-19 14:50:08 +0530

    added a main file
    this will have the experiment working of the model in the environment

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('this is the repo for environment for mango bot')
+
+from model import Decision_Model
+from env import Env
+# Lets run it here
+
+if __name__ == "__main__":
+    print('lets go')
+    # task = input("Enter the prompt: ")
+    task = "Draw a circle with diameter 10 starting from 20 units above the current location"
+    rl_env = Env(task)
+
+    
+    model = Decision_Model()
